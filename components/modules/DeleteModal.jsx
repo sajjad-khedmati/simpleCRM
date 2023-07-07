aad10e3b3da9583f1a93b2c5f051e7fe9658a24f
Sajika1 <sjdkhedmati1@gmail.com>
2023-07-07 18:17:47 +0430

    deleted customer - connected to api

--- components/modules/DeleteModal.jsx
+++ components/modules/DeleteModal.jsx
@@ -1,6 +1,22 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-hot-toast";
 import { MdOutlineClose } from "react-icons/md";
 
 export default function DeleteModal({ customer, setIsOpen }) {
+	const router = useRouter();
+	const handleDelete = async () => {
+		try {
+			const result = await axios.delete(`/api/customer/${customer._id}`);
+			if (result) {
+				toast.success(result.data.message);
+				router.push("/main");
+			}
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
+
 	return (
 		<div className="fixed top-0 left-0 w-screen h-screen bg-black/60 flex items-center justify-center">
 			<div>
@@ -15,6 +31,7 @@ export default function DeleteModal({ customer, setIsOpen }) {
 						{customer.username}
 					</h2>
 					<button
+						onClick={handleDelete}
 						className="bg-red-400 hover:bg-red-500 text-red-50 rounded-xl py-2 px-6 w-full
                     transition-all duration-300"
 					>
